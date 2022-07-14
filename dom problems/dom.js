1da99ad8c096a47eefc8dbc1239838407ee88ee1
Gio <18yxmoreno@gmail.com>
2022-07-14 12:31:59 -0400

    last changes for first dom problem

--- dom problems/dom.js
+++ dom problems/dom.js
@@ -1,14 +1,15 @@
 //Create a basic HTML page with a P element and a button on it.
 //Then, write a JavaScript function to set the background color of that paragraph using an 
 //"onclick" event added to a button in the UI
-const paragraph = document.getElementById("pg")
 
   function changeColor() {
+    const paragraph = document.getElementById("pg")
     paragraph.style.backgroundColor = "red";
 }
 
 /*
 I accesed the p element with the id method and stored it in a variable called paragraph. Within my changeColor function I changed the style property
-and changed the background color to red. 
+and changed the background color to red.
+*/ 
 
   
