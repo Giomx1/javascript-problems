a3ea5dc33a9e692ee644ff236d3d5c6d048b59cf
Gio <18yxmoreno@gmail.com>
2022-07-31 11:44:14 -0400

    objects problem one

--- javascript array problems/array4.js
+++ javascript array problems/array4.js
@@ -1,13 +1,27 @@
 const myNumbers = [1,1,1,2,2,2,2,2,33];//array of numbers with duplicates
 
 function removeDuplicates(array) {//function with variable removeDuplicates with parameter of array
-  let uniqueArray = [];// 
+  let uniqueArray = [];// only storing unique values here
   
   for (let i = 0; i < array.length; i++) {//looping through array
-    if(!uniqueArray.includes(array[i])) {//if statement 
+    console.log(array[i]); //prints all the numbers in the array
+    if(!uniqueArray.includes(array[i])) {
       uniqueArray.push(array[i]);
     }
   }
   return uniqueArray;//I return this variable because I want the value of this to be returned when the function is called.
 }
 console.log(removeDuplicates(myNumbers));// printing my results of removed duplicates
+/*
+It’s “unique” if it’s not included in the new array already.
+ if(!uniqueArray.includes(array[i])) {//if statement 
+      uniqueArray.push(array[i]);
+
+If the item in the loop is already included in the array, 
+
+then we do not want to add it again, because then it would have duplicates, and wouldn’t be unique.
+
+Instead of thinking in terms of “unique” 
+
+just think that we only want to add one of each item to our array. If an item is already in there, we don’t want to add/push it in there again
+*/
--- object-problems/object1.js
+++ object-problems/object1.js
@@ -1,1 +1,17 @@
-object1.js
+/*
+Create a sample object, then write a JavaScript function that deletes the property of an object passed to it.
+
+Your JavaScript function should take in two parameters, the object and the key/property you want to delete.
+*/
+
+const car = {// My object with properties and values
+  engine: "v8",
+  cylinders: 8,
+  mpg: 23,
+  model: "Ford Mustang",
+}
+function deleteProperty (object,property) {// my function with variable of deleteKey with two parameters
+  delete object[property]// Here I use the delete property to delete a property out of my object
+  return object //Even though I delete the property I still want to return the rest of my object
+}
+console.log(deleteProperty(car,'mpg'));// Here I log my result and remove the "mpg" property
